a938ec144f225c38fb6425a5eaf2a7f02e69e735
noora83 <nja5066@rit.edu>
2025-01-29 21:38:41 +0400

    Update McqsQuestion.java

--- McqsQuestion.java
+++ McqsQuestion.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 import java.util.Arrays; // Added for array comparison
 
+/*In this activity, we created a system (with an important question and answer choices)in which we ask a multiple choice quesion (MCQ),
+ * and the user is given 3 choices to choose from.
+ * The user inputs his/hers choice, and is immediately shown whether the answer was correct or false.
+ */
 public class McqsQuestion {
     private String question;
     private String[] answer;
     private String correctAnswer;
 
     /**
+     * initializes the question, answer options, and the correct answer.
      * 
-     * @param question
-     * @param answer
-     * @param correctAnswer
+     * @param question      The question
+     * @param answer        An array of the answer choices
+     * @param correctAnswer The correct answer to the question
      */
     public McqsQuestion(String question, String[] answer, String correctAnswer) {
         this.answer = answer;
@@ -19,37 +24,81 @@ public class McqsQuestion {
     }
 
     /* --------------------------- getters and setters -------------------------- */
+    /**
+     * Gets the value of the question
+     * 
+     * @return String of the question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Sets the question
+     * 
+     * @param question the new question
+     */
     public void setQuestion(String question) {
         this.question = question;
     }
 
+    /**
+     * Gets an array of the answer options (strings)
+     * 
+     * @return string input for array of answers
+     */
     public String[] getAnswer() {
         return answer;
     }
 
+    /**
+     * Sets the answer choices
+     * 
+     * @param answer the new answers
+     */
     public void setAnswer(String[] answer) {
         this.answer = answer;
     }
 
+    /**
+     * Gets the value of the correct answer
+     * 
+     * @return string of the correct answer
+     */
     public String getCorrectAnswer() {
         return correctAnswer;
     }
 
+    /**
+     * Sets the correct answer
+     * 
+     * @param correctAnswer the new correct answer
+     */
     public void setCorrectAnswer(String correctAnswer) {
         this.correctAnswer = correctAnswer;
     }
 
     /* -------------------------------- toString -------------------------------- */
+    /**
+     * We wrote this to set up the area for the question followed by the multiple
+     * choice answers on a new line.
+     * This shows the user the question, choices, and asks for the answer.
+     */
     @Override
     public String toString() {
         return question + "\n" + Arrays.toString(answer); // Fixed: Proper array printing
     }
 
     /* --------------------------------- equals --------------------------------- */
+
+    /**
+     * This compares if the user's input is equal to the correct answer, or in other
+     * words, is correct.
+     * Checks if the objects question is the same, answers are the same, and if the
+     * correct answer remains the same; and then returns true if everything is the
+     * same.
+     * Or else it would return false
+     */
     public boolean equals(Object obj) {
         if (obj instanceof McqsQuestion) {
             McqsQuestion other = (McqsQuestion) obj;
@@ -64,8 +113,11 @@ public class McqsQuestion {
 
     /* ----------------------- creating the main function ----------------------- */
     /**
+     * It displays the question, as well as the multiple choice options, and checks
+     * the answer inputted by the user.
      * 
-     * @param args
+     * @param args question, answer choices, and String of the feedback (correct
+     *             answer).
      */
     public static void main(String[] args) {
         String question = "What is the color of the sky?";
